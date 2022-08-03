0e1a51f47d5b749669a895136e2ed549c656118b
t-amberwang <t-amberwang@microsoft.com>
2022-08-03 15:13:36 -0700

    add machine id to prometheus

--- app.js
+++ app.js
@@ -3,33 +3,26 @@ var express = require('express');
 var app = express();
 var PORT = 3001;
 
-const { collectDefaultMetrics, register } = require('prom-client');
+const { register } = require('prom-client');
 const { Counter } = require('prom-client');
-
-collectDefaultMetrics({
-    timeout: 10000,
-    gcDurationBuckets: [0.001, 0.01, 0.1, 1, 2, 5], // These are the default buckets.
-});
+const { machineIdSync } = require('node-machine-id')
 
 const requests = new Counter({
     name: 'requests',
-    help: 'count 200s and 500s',
-    labelNames: ['statusCode'],
+    help: 'count 200s, 300s and 500s',
+    labelNames: ['statusCode', 'machineID'],
 })
 
-// const c500 = new Counter({
-//     name: 'c500',
-//     help: 'count 500s'
-// })
+const mid = machineIdSync()
 
 app.get('/', function(req, res) {
     res.status(200).send('Hello world');
-    requests.inc({statusCode: 200})
+    requests.inc({statusCode: 200, machineID: mid})
 });
 
 app.get('/error', function(req, res) {
     res.status(500).send('This webpage returns a 500 status!');
-    requests.inc({statusCode: 500})
+    requests.inc({statusCode: 500, machineID: mid})
 });
 
 app.get('/metrics', function(req, res) {
